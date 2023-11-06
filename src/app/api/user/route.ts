0cd5106499926787589b443bdfed55a0cc8ea239
Leindfraust <rozer223@gmail.com>
2023-11-06 21:51:33 +0800

    refactor: route GET for users

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,8 +2,82 @@ import { NextResponse } from "next/server";
 import { authConfig } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/db";
 import { getServerSession } from "next-auth";
+import { User } from "@prisma/client";
 //Promise<any> is a temporary fix
-export async function PATCH(req: Request): Promise<any>  {
+export async function GET(req: Request): Promise<any> {
+    const url = new URL(req.url)
+    const lastCursor = url.searchParams.get("cursor")
+    const keyword = url.searchParams.get("q")?.split(" ").join("&")
+
+    const prismaQuery = {
+        where: {
+            name: {
+                search: keyword
+            },
+            username: {
+                search: keyword
+            },
+            bio: {
+                search: keyword
+            },
+            address: {
+                search: keyword
+            },
+            occupation: {
+                search: keyword
+            }
+        }
+    }
+
+    try {
+        const users = await prisma.user.findMany({
+            ...prismaQuery,
+            ...(lastCursor && {
+                skip: 1,
+                cursor: {
+                    id: lastCursor,
+                }
+            }),
+            take: 1
+        })
+
+        if (users.length === 0) {
+            return NextResponse.json({
+                data: [],
+                metaData: {
+                    lastCursor: null,
+                    hasNextPost: false
+                },
+            }, { status: 200 })
+        }
+
+        const lastPost: User = users[users.length - 1];
+        const cursor: string = lastPost.id
+
+        const nextPost = await prisma.user.findMany({
+            ...prismaQuery,
+            take: 1,
+            skip: 1,
+            cursor: {
+                id: cursor
+            }
+        });
+
+        const data = {
+            data: users,
+            metaData: {
+                lastCursor: cursor !== undefined ? cursor : null,
+                hasNextPost: nextPost.length > 0
+            }
+        }
+
+        return NextResponse.json({ data }, { status: 200 })
+    } catch (err) {
+        return NextResponse.json({ err }, { status: 500 })
+    }
+}
+
+export async function PATCH(req: Request): Promise<any> {
     const body = await req.json()
     const session = await getServerSession(authConfig)
     try {
@@ -13,13 +87,14 @@ export async function PATCH(req: Request): Promise<any>  {
                 ...body
             }
         })
-        const updateBlogs = await prisma.blog.updateMany({
+        const updatePosts = await prisma.post.updateMany({
             where: { userId: session?.user.id },
             data: {
-                author: body.name
+                author: body.name,
+                authorUsername: body.username
             }
         })
-        if (user && updateBlogs) {
+        if (user && updatePosts) {
             return NextResponse.json({ status: 200 })
         }
     } catch (err) {

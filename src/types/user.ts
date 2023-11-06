8502e610e383122164797bd916487e1b066be91c
Leindfraust <rozer223@gmail.com>
2023-11-06 21:36:58 +0800

    refactor: delete types as it's not relevant anymore

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,25 +1,6 @@
-type DisplayImageProps = {
-    id: string
-    name: string
-    image: string
-    session: boolean
-}
-
-type EditUserDetailsProps = {
-    vanityUrl?: string
-    name: string
-    bio?: string
-    address?: string
-    occupation?: string,
-    email: string
-    socials?: UserSocials[]
-    viewsVisibility: boolean;
-}
-
-
 type UserSocials = {
     name: string,
     url: string
 }
 
-export type { DisplayImageProps, EditUserDetailsProps, UserSocials }
+export type { UserSocials }

4904ceb87c60f0a214cd247a6b558bb529e2464d
Leindfraust <rozer223@gmail.com>
2023-11-06 21:52:21 +0800

    feat: tag ranking API

---
+++ src/app/api/tag/ranking/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { TagRank } from "@/types/tag";
+import prisma from "@/db";
+
+export async function GET(req?: NextRequest): Promise<any> {
+    const url = new URL(req?.url ?? '')
+    const keyword = url && url.searchParams.get('q')
+    const tagRankings = await prisma.tagsRanking.findFirst({
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    const response = async () => {
+        if (keyword) {
+            return (tagRankings?.data as TagRank[]).filter((tag: TagRank) => tag.tag.toLowerCase().includes(keyword))
+        }
+        return tagRankings?.data
+    }
+
+
+    return NextResponse.json(await response(), { status: 200 })
+
+}

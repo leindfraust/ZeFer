89097b0235e9874621c0b7b44d554a3118b5a1be
Leindfraust <rozer223@gmail.com>
2023-11-06 21:50:23 +0800

    Create page.tsx

---
+++ src/app/new/page.tsx
@@ -0,0 +1,23 @@
+import Tiptap from "@/components/wysiwyg/Tiptap";
+import { getServerSession } from "next-auth";
+import { authConfig } from "../api/auth/[...nextauth]/route";
+import prisma from "@/db";
+import { PostDraft } from "@prisma/client";
+
+
+export default async function CreatePost() {
+    const session = await getServerSession(authConfig)
+    const user = await prisma.user.findUnique({
+        where: { id: session?.user.id },
+        select: {
+            id: true,
+            username: true,
+            draft: true
+        }
+    })
+    // console.log(user?.draft)
+    const tags = await import('../api/tag/route');
+
+    return (<Tiptap userId={user?.id} username={user?.username} postDraft={user?.draft as PostDraft ?? ''} tags={[...await (await tags.GET()).json()]} />)
+
+}

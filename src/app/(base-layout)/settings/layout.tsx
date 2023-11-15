a430c8738ebb307df60e786d427c5e1ebf2409f7
Leindfraust <rozer223@gmail.com>
2023-11-15 22:01:41 +0800

    fix: removed route causing vercel to fail
    idk why is that there(?)

--- src/app/(base-layout)/settings/layout.tsx
+++ src/app/(base-layout)/settings/layout.tsx
@@ -8,8 +8,7 @@ export const metadata: Metadata = {
 export default async function SettingsLayout({
     children,
 }: {
-    children: React.ReactNode,
-    route: string
+    children: React.ReactNode
 }) {
     return (<div className="mt-12 mb-12 lg:mr-28 lg:ml-28 mx-auto">
         <div className="lg:flex justify-center">

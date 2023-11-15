d46798b0fcfb77f8bbe27d903fabb53f914779a9
Leindfraust <rozer223@gmail.com>
2023-11-15 21:45:39 +0800

    feat: added management of series

--- src/components/post/PostManageTable.tsx
+++ src/components/post/PostManageTable.tsx
@@ -14,7 +14,7 @@ export default function PostManageTable() {
     const pathName = usePathname()
     const searchParams = useSearchParams()
     const deleteModalWarnRef = useRef<HTMLDialogElement>(null)
-    const [selectedPostIdDeletion, setSelectedPostIdDeletion] = useState<string>('')
+    const [selectedPostIdForDeletion, setSelectedPostIdForDeletion] = useState<string>('')
     const [orderBy, setOrderBy] = useState<'recent' | 'unpublished' | 'most-views' | 'most-reactions' | 'most-comments'>(searchParams.get('sort') as 'recent' | 'unpublished' | 'most-views' | 'most-reactions' | 'most-comments')
     const { replace } = useRouter()
 
@@ -28,32 +28,48 @@ export default function PostManageTable() {
         return data
     }
 
-    function handleModalWarn(decision: 'delete' | 'cancel') {
+    function handleDeleteModalWarn(decision: 'delete' | 'cancel') {
         if (decision === 'delete') {
             mutationDeletePost.mutate()
         }
         if (decision === 'cancel') {
-            setSelectedPostIdDeletion('')
+            setSelectedPostIdForDeletion('')
         }
     }
 
     const deletePost = async () => {
         const params = new URLSearchParams({
-            postId: selectedPostIdDeletion
+            postId: selectedPostIdForDeletion
         })
         const response = await fetch(`/api/post/manage?${params}`, {
             method: "DELETE"
         })
         const json = await response.json()
-        if (json) setSelectedPostIdDeletion('')
+        if (json) setSelectedPostIdForDeletion('')
         return json
     }
+
+    const publishOrUnpublishPost = async (postId: string, publish: 'true' | 'false') => {
+        const params = new URLSearchParams({
+            postId: postId,
+            publish: publish
+        })
+        const response = await fetch(`/api/post/manage?${params}`, {
+            method: "PUT"
+        })
+        const json = await response.json()
+        return json
+    }
+
+    const mutationPublishOrUnpublishPost = useMutation({
+        mutationFn: ({ postId, publish }: { postId: string, publish: 'true' | 'false' }) => publishOrUnpublishPost(postId, publish)
+    })
 
     const mutationDeletePost = useMutation({
         mutationFn: deletePost
     })
 
-    const { data, refetch, isSuccess } = useQuery({
+    const { data, refetch, isSuccess, isLoading, isRefetching } = useQuery({
         queryKey: ['posts'],
         queryFn: getPosts
     })
@@ -62,10 +78,12 @@ export default function PostManageTable() {
         if (!orderBy) setOrderBy(searchParams.get('sort') as 'recent' | 'unpublished' | 'most-views' | 'most-reactions' | 'most-comments')
         replace(`${pathName}?sort=${orderBy ?? 'recent'}`, { scroll: false })
         if (mutationDeletePost.isSuccess) refetch()
+        if (mutationPublishOrUnpublishPost.isSuccess) refetch()
         refetch()
-    }, [mutationDeletePost.isSuccess, orderBy, pathName, refetch, replace, searchParams])
+    }, [mutationDeletePost.isSuccess, mutationPublishOrUnpublishPost.isPending, mutationPublishOrUnpublishPost.isSuccess, orderBy, pathName, refetch, replace, searchParams])
 
     return (<div className="container">
+
         <dialog className="modal" ref={deleteModalWarnRef}>
             <div className="modal-box">
                 <h3 className="font-bold text-lg">Delete Post</h3>
@@ -74,8 +92,8 @@ export default function PostManageTable() {
                     <form method="dialog">
                         {/* if there is a button in form, it will close the modal */}
                         <div className="flex justify-center gap-4">
-                            <button className="btn btn-error" onClick={() => handleModalWarn('delete')}>Delete</button>
-                            <button className="btn" onClick={() => handleModalWarn('cancel')}>Close</button>
+                            <button className="btn btn-error" onClick={() => handleDeleteModalWarn('delete')}>Delete</button>
+                            <button className="btn" onClick={() => handleDeleteModalWarn('cancel')}>Close</button>
                         </div>
                     </form>
                 </div>
@@ -105,10 +123,32 @@ export default function PostManageTable() {
                                     <div className="flex justify-end items-center gap-4">
                                         <button className="btn btn-xs btn-outline btn-error" onClick={() => {
                                             deleteModalWarnRef.current?.show()
-                                            setSelectedPostIdDeletion(post.id)
+                                            setSelectedPostIdForDeletion(post.id)
                                         }}>Delete</button>
                                         <Link href={`/${post.authorUsername || post.userId}/${post.titleId}/edit`} className="btn btn-xs btn-outline btn-primary">Edit</Link>
-                                        <FontAwesomeIcon icon={faEllipsis} className="cursor-pointer" size="lg" />
+
+                                        <div className="dropdown dropdown-left">
+                                            <label tabIndex={0}><FontAwesomeIcon icon={faEllipsis} className="cursor-pointer" size="lg" /></label>
+                                            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                                                <li><button>Statistics</button></li>
+                                                {post.published ? (
+                                                    <li><button onClick={async () => {
+                                                        mutationPublishOrUnpublishPost.mutate({
+                                                            postId: post.id,
+                                                            publish: 'false'
+                                                        })
+                                                    }}>Unpublish</button></li>
+                                                ) : (
+                                                    <li><button onClick={async () => {
+                                                        mutationPublishOrUnpublishPost.mutate({
+                                                            postId: post.id,
+                                                            publish: 'true'
+                                                        })
+                                                    }}>Publish</button></li>
+                                                )}
+                                            </ul>
+                                        </div>
+
                                     </div>
                                 </td>
                             </tr>
@@ -116,6 +156,11 @@ export default function PostManageTable() {
                     ))}
                 </tbody>
             </table>
+            {data === undefined || (data as Array<typeof data>).length === 0 && !isLoading && !isRefetching && (<>
+                <div className="container">
+                    <p className="text-xl font-bold">No results were found.</p>
+                </div>
+            </>)}
         </div>
     </div>)
 }

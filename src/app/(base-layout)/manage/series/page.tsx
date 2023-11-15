bfa15826cc20a9dc251ec3218da8fe43533ed191
Leindfraust <rozer223@gmail.com>
2023-11-15 21:48:04 +0800

    Create page.tsx

---
+++ src/app/(base-layout)/manage/series/page.tsx
@@ -0,0 +1,10 @@
+import QueryWrapper from "@/components/QueryWrapper";
+import SeriesManageContainer from "@/components/SeriesManageContainer";
+
+export default function ManageSeries() {
+    return (<>
+        <QueryWrapper>
+            <SeriesManageContainer />
+        </QueryWrapper>
+    </>)
+}

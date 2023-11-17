8121b3edff35e8a4853ac10e53235bfb18f86530
Leindfraust <rozer223@gmail.com>
2023-11-17 21:10:56 +0800

    feat: added vercel analytics also removed commented code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -23,32 +24,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        {/* <footer className="footer p-10 bg-base-200 text-base-content">
-          <aside>
-            <h1 className=' text-7xl'>ZeFer</h1>
-            <p>Tell your story to the world.</p>
-          </aside>
-          <nav>
-            <header className="footer-title">Services</header>
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
-          </nav>
-          <nav>
-            <header className="footer-title">Company</header>
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
-          </nav>
-          <nav>
-            <header className="footer-title">Legal</header>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
-          </nav>
-        </footer> */}
+        <Analytics />
       </body>
     </html>
   )

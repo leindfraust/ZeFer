9e7585b9c4294f6bdd98bdede9cc0f8c6909368f
Leindfraust <rozer223@gmail.com>
2023-11-25 15:57:05 +0800

    style: prettier

--- src/components/wysiwyg/menu/MenuItems.tsx
+++ src/components/wysiwyg/menu/MenuItems.tsx
@@ -2,13 +2,23 @@ import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { MenuItemProps } from "@/types/menu";
 
-export default function MenuItems({ icon, title, action, isActive }: MenuItemProps) {
-
-    return (<>
-        <button className={`btn btn-ghost ${isActive && isActive() ? 'btn-active' : ''}`}
-            onClick={action}
-            title={title}>
-            <FontAwesomeIcon icon={icon as IconDefinition} />
-        </button>
-    </>)
+export default function MenuItems({
+    icon,
+    title,
+    action,
+    isActive,
+}: MenuItemProps) {
+    return (
+        <>
+            <button
+                className={`btn btn-ghost ${
+                    isActive && isActive() ? "btn-active" : ""
+                }`}
+                onClick={action}
+                title={title}
+            >
+                <FontAwesomeIcon icon={icon as IconDefinition} />
+            </button>
+        </>
+    );
 }

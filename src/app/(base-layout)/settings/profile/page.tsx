866b41d8d913640de03236ef918564b42f79a2da
Leindfraust <rozer223@gmail.com>
2023-11-25 16:08:12 +0800

    fix: resolve module not found

--- src/app/(base-layout)/settings/profile/page.tsx
+++ src/app/(base-layout)/settings/profile/page.tsx
@@ -1,6 +1,6 @@
 import prisma from "@/db";
 import { authConfig } from "@/utils/authConfig";
-import ProfileSettingsComponent from "@/components/settings/ProfileSettings";
+import ProfileSettingsComponent from "@/components/settings/Profile";
 import { User } from "@prisma/client";
 import { getServerSession } from "next-auth";
 

83a61643cc816d9fc9e4e8fc3ce8ed22c852949b
Leindfraust <rozer223@gmail.com>
2023-11-25 15:55:25 +0800

    style: prettier

--- src/components/user/actions/UserFollowButton.tsx
+++ src/components/user/actions/UserFollowButton.tsx
@@ -1,19 +1,35 @@
-"use client"
+"use client";
 
 import { toggleFollowUser } from "@/utils/actions/user";
 import { useState } from "react";
 
-export default function UserFollowButton({ userId, initialFollowStatus }: { userId: string, initialFollowStatus: boolean }) {
-    const [userFollowStatus, setUserFollowStatus] = useState<boolean>(initialFollowStatus)
+export default function UserFollowButton({
+    userId,
+    initialFollowStatus,
+}: {
+    userId: string;
+    initialFollowStatus: boolean;
+}) {
+    const [userFollowStatus, setUserFollowStatus] =
+        useState<boolean>(initialFollowStatus);
 
     async function updateFollowUserStatus() {
-        const response = await toggleFollowUser(userId)
-        if (response === 'following') {
-            setUserFollowStatus(true)
+        const response = await toggleFollowUser(userId);
+        if (response === "following") {
+            setUserFollowStatus(true);
         }
-        if (response === 'unfollowing') {
-            setUserFollowStatus(false)
+        if (response === "unfollowing") {
+            setUserFollowStatus(false);
         }
     }
-    return (<button className={`btn ${userFollowStatus ? 'btn-primary' : 'btn-outline'}`} onClick={updateFollowUserStatus}>{userFollowStatus ? 'Following' : 'Follow'}</button>)
+    return (
+        <button
+            className={`btn ${
+                userFollowStatus ? "btn-primary" : "btn-outline"
+            }`}
+            onClick={updateFollowUserStatus}
+        >
+            {userFollowStatus ? "Following" : "Follow"}
+        </button>
+    );
 }

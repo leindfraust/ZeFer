08d7ecbdaa1107e394605744f54f10009d392d75
Leindfraust <rozer223@gmail.com>
2023-11-25 15:45:47 +0800

    chore: added headers for socket io

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    const env = process.env.NODE_ENV
+    let URL = "http://localhost:5000"
+    if (env === 'production') URL = "https://melted-patience-leindfraust.koyeb.app/"
+    return [
+      {
+        source: "/socket.io",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: URL },
+          { key: "Access-Control-Allow-Methods", value: "GET, DELETE, PATCH, POST, PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  }
+
+  ,
   images: {
     remotePatterns: [
       {

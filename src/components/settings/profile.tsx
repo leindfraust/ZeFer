a7e6d7ef182ad38a68354502f472e1a80890a215
Leindfraust <rozer223@gmail.com>
2023-11-25 15:55:10 +0800

    style: prettier

--- src/components/settings/profile.tsx
+++ src/components/settings/profile.tsx
@@ -1,134 +1,151 @@
-'use client'
+"use client";
 
-import { UserSocials } from '@/types/user'
-import { useRouter } from 'next/navigation'
-import { FormContext } from '@/types/formContext'
-import { FormProvider, useForm } from 'react-hook-form'
-import Input from '@/components/Input'
-import { Fragment } from 'react'
-import { User } from '@prisma/client'
-import DisplayImage from '../cloudinary/user/DisplayImage'
+import { UserSocials } from "@/types/user";
+import { useRouter } from "next/navigation";
+import { FormContext } from "@/types/formContext";
+import { FormProvider, useForm } from "react-hook-form";
+import Input from "@/components/Input";
+import { Fragment } from "react";
+import { User } from "@prisma/client";
+import DisplayImage from "../cloudinary/user/DisplayImage";
 
-export default function ProfileSettingsComponent({ username, name, bio, address, occupation, email, socials, id, image }: User) {
-    const router = useRouter()
-    const socialData = [...socials] as UserSocials[]
+export default function ProfileSettingsComponent({
+    username,
+    name,
+    bio,
+    address,
+    occupation,
+    email,
+    socials,
+    id,
+    image,
+}: User) {
+    const router = useRouter();
+    const socialData = [...socials] as UserSocials[];
     const uploadImgProps = {
         id,
         image,
-        name
-    } as User
+        name,
+    } as User;
 
     const socialForms: FormContext[] = [
         {
-            name: 'Facebook',
-            type: 'text',
-            placeholder: 'Your facebook link',
-            value: socialData.find(social => social.name === 'Facebook')?.url ? socialData.find(social => social.name === 'Facebook')?.url : '',
+            name: "Facebook",
+            type: "text",
+            placeholder: "Your facebook link",
+            value: socialData.find((social) => social.name === "Facebook")?.url
+                ? socialData.find((social) => social.name === "Facebook")?.url
+                : "",
             required: {
                 value: false,
-                message: ''
+                message: "",
             },
         },
         {
-            name: 'Twitter',
-            type: 'text',
-            placeholder: 'Your twitter link',
-            value: socialData.find(social => social.name === 'Twitter')?.url ? socialData.find(social => social.name === 'Twitter')?.url : '',
+            name: "Twitter",
+            type: "text",
+            placeholder: "Your twitter link",
+            value: socialData.find((social) => social.name === "Twitter")?.url
+                ? socialData.find((social) => social.name === "Twitter")?.url
+                : "",
             required: {
                 value: false,
-                message: ''
-            }
-        },
-        {
-            name: 'Personal Website',
-            type: 'text',
-            placeholder: 'Your personal website link',
-            value: socialData.find(social => social.name === 'Personal Website')?.url ? socialData.find(social => social.name === 'Personal Webite')?.url : '',
-            required: {
-                value: false,
-                message: ''
+                message: "",
             },
         },
-    ]
-    const submissions = useForm()
+        {
+            name: "Personal Website",
+            type: "text",
+            placeholder: "Your personal website link",
+            value: socialData.find(
+                (social) => social.name === "Personal Website"
+            )?.url
+                ? socialData.find((social) => social.name === "Personal Webite")
+                      ?.url
+                : "",
+            required: {
+                value: false,
+                message: "",
+            },
+        },
+    ];
+    const submissions = useForm();
 
     const username_validation: FormContext = {
-        name: 'Username',
-        type: 'text',
-        placeholder: 'Your unique username',
-        value: username ? username : '',
+        name: "Username",
+        type: "text",
+        placeholder: "Your unique username",
+        value: username ? username : "",
         required: {
             value: false,
-            message: 'This field is not required'
-        }
-    }
+            message: "This field is not required",
+        },
+    };
 
     const name_validation: FormContext = {
-        name: 'Name',
-        type: 'text',
-        placeholder: 'Your display name',
-        value: name ? name : '',
+        name: "Name",
+        type: "text",
+        placeholder: "Your display name",
+        value: name ? name : "",
         required: {
             value: true,
-            message: 'This field is required'
-        }
-    }
+            message: "This field is required",
+        },
+    };
 
     const bio_validation: FormContext = {
-        name: 'Bio',
-        type: 'textarea',
-        value: bio ? bio : '',
-        placeholder: 'Describe who you are...',
-        maxLength: {
-            value: 500,
-            message: 'You are exceeding the required limit.'
-        },
+        name: "Bio",
+        type: "textarea",
+        value: bio ? bio : "",
+        placeholder: "Describe who you are...",
         required: {
             value: false,
-            message: ''
-        }
-    }
+            message: "",
+        },
+    };
 
     const address_validation: FormContext = {
-        name: 'Address',
-        type: 'text',
-        placeholder: 'Your address',
-        value: address ? address : '',
+        name: "Address",
+        type: "text",
+        placeholder: "Your address",
+        value: address ? address : "",
         required: {
             value: false,
-            message: ''
-        }
-    }
+            message: "",
+        },
+    };
 
     const email_validation: FormContext = {
-        name: 'Email',
-        type: 'text',
-        placeholder: 'Your email',
-        value: email ? email : '',
+        name: "Email",
+        type: "text",
+        placeholder: "Your email",
+        value: email ? email : "",
         required: {
             value: true,
-            message: 'This field is required'
-        }
-    }
+            message: "This field is required",
+        },
+    };
 
     const occupation_validation: FormContext = {
-        name: 'Occupation',
-        type: 'text',
-        placeholder: 'Your job right now...',
-        value: occupation ? occupation : '',
+        name: "Occupation",
+        type: "text",
+        placeholder: "Your job right now...",
+        value: occupation ? occupation : "",
         required: {
             value: false,
-            message: ''
-        }
-    }
+            message: "",
+        },
+    };
 
-    const updateDetails = submissions.handleSubmit(async data => {
-        let socials: UserSocials[] = []
-        socialForms.forEach(social => socials.push({
-            name: social.name,
-            url: data[social.name]
-        }))
-        const update = await fetch('/api/user', {
+    const updateDetails = submissions.handleSubmit(async (data) => {
+        let socials: UserSocials[] = [];
+        socialForms.forEach((social) =>
+            socials.push({
+                name: social.name,
+                url: data[social.name],
+            })
+        );
+        const update = await fetch("/api/user", {
             method: "PATCH",
             body: JSON.stringify({
                 username: data.Username,
@@ -137,39 +154,44 @@ export default function ProfileSettingsComponent({ username, name, bio, address,
                 address: data.Address,
                 occupation: data.Occupation,
                 email: data.Email,
-                socials: socials
-            })
-        })
+                socials: socials,
+            }),
+        });
         if (update.ok) {
-            router.refresh()
+            router.refresh();
         }
-    })
+    });
 
-
-
-    return (<>
-        <div className="mx-auto lg:w-9/12 justify-center space-y-6">
-            <FormProvider {...submissions}>
-                <div className='shadow-lg p-12 rounded-md space-y-2'>
-                    <h3 className='text-2xl font-bold'>Profile Information</h3>
-                    <Input {...username_validation} />
-                    <Input {...name_validation} />
-                    <Input {...bio_validation} />
-                    <Input {...address_validation} />
-                    <Input {...occupation_validation} />
-                    <Input {...email_validation} />
-                    <DisplayImage {...uploadImgProps} />
-                </div>
-                <div className='shadow-lg p-12 rounded-md space-y-2'>
-                    <h3 className='text-2xl font-bold'>Social Links</h3>
-                    {socialForms.length !== 0 && socialForms.map(social => (
-                        <Fragment key={social.name}>
-                            <Input {...social} />
-                        </Fragment>
-                    ))}
-                </div>
-            </FormProvider>
-            <button className='btn btn-info w-full' onClick={updateDetails}>Update Profile Information</button>
-        </div>
-    </>)
+    return (
+        <>
+            <div className="mx-auto w-9/12 justify-center space-y-6">
+                <FormProvider {...submissions}>
+                    <div className="shadow-lg p-12 rounded-md space-y-2">
+                        <h3 className="text-2xl font-bold">
+                            Profile Information
+                        </h3>
+                        <Input {...username_validation} />
+                        <Input {...name_validation} />
+                        <Input {...bio_validation} />
+                        <Input {...address_validation} />
+                        <Input {...occupation_validation} />
+                        <Input {...email_validation} />
+                        <DisplayImage {...uploadImgProps} />
+                    </div>
+                    <div className="shadow-lg p-12 rounded-md space-y-2">
+                        <h3 className="text-2xl font-bold">Social Links</h3>
+                        {socialForms.length !== 0 &&
+                            socialForms.map((social) => (
+                                <Fragment key={social.name}>
+                                    <Input {...social} />
+                                </Fragment>
+                            ))}
+                    </div>
+                </FormProvider>
+                <button className="btn btn-info w-full" onClick={updateDetails}>
+                    Update Profile Information
+                </button>
+            </div>
+        </>
+    );
 }

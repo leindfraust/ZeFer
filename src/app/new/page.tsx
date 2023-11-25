c27660906301ba60c8d73a0ecbd308570d2591a1
Leindfraust <rozer223@gmail.com>
2023-11-25 15:53:57 +0800

    style: prettier

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -4,20 +4,24 @@ import { authConfig } from "@/utils/authConfig";
 import prisma from "@/db";
 import { PostDraft } from "@prisma/client";
 
-
 export default async function CreatePost() {
-    const session = await getServerSession(authConfig)
+    const session = await getServerSession(authConfig);
     const user = await prisma.user.findUnique({
         where: { id: session?.user.id },
         select: {
             id: true,
             username: true,
-            draft: true
-        }
-    })
-    // console.log(user?.draft)
-    const tags = await import('../api/tag/route');
+            draft: true,
+        },
+    });
+    const tags = await import("../api/tag/route");
 
-    return (<Tiptap userId={user?.id} username={user?.username} postDraft={user?.draft as PostDraft ?? ''} tags={[...await (await tags.GET()).json()]} />)
-
+    return (
+        <Tiptap
+            userId={user?.id}
+            username={user?.username}
+            postDraft={(user?.draft as PostDraft) ?? ""}
+            tags={[...(await (await tags.GET()).json())]}
+        />
+    );
 }

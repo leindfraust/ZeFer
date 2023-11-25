b2dd78eeea59c962423de824fa5fe1de9d84c18d
Leindfraust <rozer223@gmail.com>
2023-11-25 15:51:48 +0800

    feat: added zefer analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ZeFer',
-  description: 'Tell your story to the world.',
-}
+    title: "ZeFer",
+    description: "Tell your story to the world.",
+};
 
 export default async function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                {children}
+                <Analytics />
+            </body>
+        </html>
+    );
 }

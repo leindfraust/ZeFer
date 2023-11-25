4647d73ff6bb8bb5510784c7766860d55b8b2028
Leindfraust <rozer223@gmail.com>
2023-11-25 15:44:38 +0800

    style: prettier

--- src/app/(base-layout)/search/tags/page.tsx
+++ src/app/(base-layout)/search/tags/page.tsx
@@ -1,9 +1,15 @@
-import QueryWrapper from "@/components/QueryWrapper"
-import TagList from "@/components/TagList"
+import QueryWrapper from "@/components/QueryWrapper";
+import TagList from "@/components/TagList";
 
-export default function SearchTags({ searchParams }: { searchParams: { q?: string } }) {
-    const keyword = searchParams.q
-    return (<QueryWrapper>
-        <TagList keyword={keyword as string} />
-    </QueryWrapper>)
+export default function SearchTags({
+    searchParams,
+}: {
+    searchParams: { q?: string };
+}) {
+    const keyword = searchParams.q;
+    return (
+        <QueryWrapper>
+            <TagList keyword={keyword as string} />
+        </QueryWrapper>
+    );
 }

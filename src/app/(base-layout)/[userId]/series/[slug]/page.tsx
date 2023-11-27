bd49abb6acfd8d4be4492147408d00b68d8d3935
Leindfraust <rozer223@gmail.com>
2023-11-28 00:33:51 +0800

    style: prettier

--- src/app/(base-layout)/[userId]/series/[slug]/page.tsx
+++ src/app/(base-layout)/[userId]/series/[slug]/page.tsx
@@ -1,11 +1,15 @@
-import PostContainer from "@/components/post/PostContainer"
-import prisma from "@/db"
-import { Metadata } from "next"
-import { notFound } from "next/navigation"
-import { Fragment } from "react"
+import PostContainer from "@/components/post/PostContainer";
+import prisma from "@/db";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { Fragment } from "react";
 
-export async function generateMetadata({ params }: { params: { userId: string, slug: string } }): Promise<Metadata> {
-    const { userId, slug } = params
+export async function generateMetadata({
+    params,
+}: {
+    params: { userId: string; slug: string };
+}): Promise<Metadata> {
+    const { userId, slug } = params;
     const user = await prisma.user.findFirst({
         where: {
             OR: [
@@ -13,26 +17,30 @@ export async function generateMetadata({ params }: { params: { userId: string, s
                     id: userId,
                 },
                 {
-                    username: userId
-                }
-            ]
+                    username: userId,
+                },
+            ],
         },
-    })
+    });
     const postSeries = await prisma.postSeries.findUnique({
         where: {
             id: slug,
-            authorId: user?.id
-        }
-    })
+            authorId: user?.id,
+        },
+    });
     return {
         title: `${user?.name}'s Series ${postSeries?.title} - ZeFer`,
         description: postSeries?.description,
-        authors: [{ name: user?.name as string }]
-    }
+        authors: [{ name: user?.name as string }],
+    };
 }
 
-export default async function SeriesUserPage({ params }: { params: { userId: string, slug: string } }) {
-    const { userId, slug } = params
+export default async function SeriesUserPage({
+    params,
+}: {
+    params: { userId: string; slug: string };
+}) {
+    const { userId, slug } = params;
     const user = await prisma.user.findFirst({
         where: {
             OR: [
@@ -40,34 +48,48 @@ export default async function SeriesUserPage({ params }: { params: { userId: str
                     id: userId,
                 },
                 {
-                    username: userId
-                }
-            ]
+                    username: userId,
+                },
+            ],
         },
-    })
+    });
     const postSeries = await prisma.postSeries.findUnique({
         where: {
             id: slug,
-            authorId: user?.id
+            authorId: user?.id,
         },
         include: {
             posts: {
                 where: {
-                    published: true
-                }
-            }
-        }
-    })
-    if (!user || !postSeries) return notFound()
+                    published: true,
+                },
+                include: {
+                    _count: {
+                        select: {
+                            reactions: true,
+                            comments: true,
+                        },
+                    },
+                },
+            },
+        },
+    });
+    if (!user || !postSeries) return notFound();
 
-    return (<>
-        <h1 className="text-2xl font-bold text-info">{user.name}&apos;s <strong>{postSeries.title}</strong> Series</h1>
-        {user && postSeries && postSeries.posts.map(post => (
-            <Fragment key={post.id}>
-                <div className="lg:mr-24 lg:ml-24">
-                <PostContainer {...post} />
-                </div>
-            </Fragment>
-        ))}
-    </>)
+    return (
+        <>
+            <h1 className="text-2xl font-bold text-info">
+                {user.name}&apos;s <strong>{postSeries.title}</strong> Series
+            </h1>
+            {user &&
+                postSeries &&
+                postSeries.posts.map((post) => (
+                    <Fragment key={post.id}>
+                        <div className="lg:mr-24 lg:ml-24">
+                            <PostContainer {...post} />
+                        </div>
+                    </Fragment>
+                ))}
+        </>
+    );
 }

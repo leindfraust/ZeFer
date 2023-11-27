f9719a2ced9086433018e3326164bc0ccb5fd2b0
Leindfraust <rozer223@gmail.com>
2023-11-28 00:35:06 +0800

    feat: create reading list page

---
+++ src/app/(base-layout)/readinglist/page.tsx
@@ -0,0 +1,47 @@
+import PostContainer from "@/components/post/PostContainer";
+import prisma from "@/db";
+import { authConfig } from "@/utils/authConfig";
+import { getServerSession } from "next-auth";
+import { Fragment } from "react";
+
+export default async function ReadingList() {
+    const session = await getServerSession(authConfig);
+    const readingList = await prisma.user.findUnique({
+        where: { id: session?.user.id },
+        select: {
+            bookMarks: {
+                include: {
+                    _count: {
+                        select: {
+                            reactions: true,
+                            comments: true,
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    return (
+        <>
+            <div className="mt-12 mb-12 lg:mr-28 lg:ml-28 mx-auto">
+                <h1 className="text-2xl font-bold text-info">Reading List</h1>
+                <div className="mt-4">
+                    {session && readingList?.bookMarks.length !== 0 ? (
+                        readingList?.bookMarks.map((post) => (
+                            <Fragment key={post.id}>
+                                <div className="lg:mr-24 lg:ml-24">
+                                    <PostContainer {...post} />
+                                </div>
+                            </Fragment>
+                        ))
+                    ) : (
+                        <p className="text-md">
+                            You do not have any reading lists yet.
+                        </p>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+}

49e44d2baf245afef30ccb9be11f13874b036647
Leindfraust <rozer223@gmail.com>
2023-11-28 00:40:21 +0800

    style: prettier and changed shadow-xl to shadow-md

--- src/components/tag/TagCard.tsx
+++ src/components/tag/TagCard.tsx
@@ -1,12 +1,17 @@
-"use client"
+"use client";
 
 import { TagRank } from "@/types/tag";
 import Link from "next/link";
 import TagFollowButton from "./actions/TagFollowButton";
 
-export default function TagCard({ tag, usage, followers, isLoggedIn }: TagRank & { isLoggedIn: boolean }) {
+export default function TagCard({
+    tag,
+    usage,
+    followers,
+    isLoggedIn,
+}: TagRank & { isLoggedIn: boolean }) {
     return (
-        <div className="container p-4 max-w-xs shadow-xl rounded-md">
+        <div className="container p-4 max-w-xs shadow-md rounded-md">
             <div className="flex gap-4 items-center justify-center">
                 <div className="break-words">
                     <Link href={`/tag/${tag}`}>
@@ -14,11 +19,13 @@ export default function TagCard({ tag, usage, followers, isLoggedIn }: TagRank &
                     </Link>
                     <div className="flex items-center gap-4">
                         <p className="text-sm text-right">{usage} Posts</p>
-                        <p className="text-sm text-right">{followers} Followers</p>
+                        <p className="text-sm text-right">
+                            {followers} Followers
+                        </p>
                     </div>
                 </div>
                 <TagFollowButton tag={tag} isLoggedIn={isLoggedIn} />
             </div>
         </div>
-    )
+    );
 }

d870fd1bf5d25878d2fc0ca311f3ed530bf2b3f5
Leindfraust <rozer223@gmail.com>
2023-11-28 00:41:10 +0800

    feat: a custom headline bg

---
+++ src/components/ZeFerBgHompage.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import zeferCss from "../components/zefer.module.css";
+import { User } from "@prisma/client";
+import { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import Navigation from "./Navigation";
+import Image from "next/image";
+
+export default function ZeFerBgHomepage({
+    isLoggedIn,
+    user,
+}: {
+    user: User;
+    isLoggedIn: boolean;
+}) {
+    const [navBackgroundTransparent, setNavBackgroundTransparent] =
+        useState<boolean>(true);
+
+    const { ref, inView } = useInView({
+        initialInView: true,
+    });
+
+    useEffect(() => {
+        if (!inView) {
+            setNavBackgroundTransparent(false);
+        } else {
+            setNavBackgroundTransparent(true);
+        }
+    }, [inView]);
+
+    return (
+        <>
+            <Navigation
+                {...user}
+                className={
+                    !isLoggedIn && navBackgroundTransparent
+                        ? zeferCss.bgzefergradient
+                        : ""
+                }
+            />
+            {!isLoggedIn && (
+                <div
+                    ref={ref}
+                    className={`hero min-h-[60vh] ${zeferCss.bgzefergradient}`}
+                >
+                    <div className="hero-content text-center">
+                        <div className="max-w-md space-y-4">
+                            <Image
+                                src={"/zefer-text.svg"}
+                                height={500}
+                                width={500}
+                                alt="zefer logo with text zefer"
+                            />
+                            <h1 className="text-xl lg:text-3xl text-white">
+                                Tell your story to the world.
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}

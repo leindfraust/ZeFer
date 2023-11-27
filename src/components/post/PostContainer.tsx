c479ecbc7fb6935e36c960fb24b5ccbcda14af24
Leindfraust <rozer223@gmail.com>
2023-11-28 00:38:55 +0800

    style: prettier

--- src/components/post/PostContainer.tsx
+++ src/components/post/PostContainer.tsx
@@ -6,37 +6,78 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PostBookmark from "./actions/PostBookmark";
 import { Fragment } from "react";
 
-export default function PostContainer({ coverImage, title, titleId, description, author, userId, authorUsername, authorImage, readPerMinute, published, tags }: Post) {
+export default function PostContainer({
+    coverImage,
+    title,
+    titleId,
+    description,
+    author,
+    userId,
+    authorUsername,
+    authorImage,
+    readPerMinute,
+    published,
+    tags,
+    _count,
+}: Post & {
+    _count?: {
+        reactions: number;
+        comments: number;
+    };
+}) {
     return (
         <div className="flex flex-wrap justify-end p-2 lg:block">
-            <Link href={`/${authorUsername ? authorUsername : userId}/${titleId}`}>
+            <Link
+                href={`/${authorUsername ? authorUsername : userId}/${titleId}`}
+            >
                 <div className="lg:grid lg:grid-cols-2 mx-auto items-center space-y-2 lg:space-y-0">
                     <div className="container space-y-4 break-words">
                         {!published && (
-                            <p className="text-sm font-extrabold text-slate-400">UNPUBLISHED</p>
+                            <p className="text-sm font-extrabold text-slate-400">
+                                UNPUBLISHED
+                            </p>
                         )}
-                        <h2 className="text-lg lg:text-xl font-bold">{title}</h2>
+                        <h2 className="text-lg lg:text-xl font-bold">
+                            {title}
+                        </h2>
                         <p className="text-sm lg:text-md">{description}</p>
                         <div className="flex gap-4">
-                            {tags && tags.map(tag => (
-                                <Fragment key={tag}>
-                                    <p className="badge badge-sm badge-neutral">{tag}</p>
-                                </Fragment>
-                            ))}
+                            {tags &&
+                                tags.map((tag) => (
+                                    <Fragment key={tag}>
+                                        <p className="badge badge-sm badge-neutral">
+                                            {tag}
+                                        </p>
+                                    </Fragment>
+                                ))}
                         </div>
                         <div className="flex gap-2 items-center">
                             <div className="avatar">
                                 <div className="rounded-full">
-                                    <Image src={authorImage} alt={author} width={25} height={25} />
+                                    <Image
+                                        src={authorImage}
+                                        alt={author}
+                                        width={25}
+                                        height={25}
+                                    />
                                 </div>
                             </div>
                             <div className="container">
-                                <p className="text-sm">{author} <strong>·</strong> {readPerMinute} min read</p>
+                                <p className="text-sm">
+                                    {author} <strong>·</strong> {readPerMinute}{" "}
+                                    min read
+                                </p>
                             </div>
                         </div>
                     </div>
                     <figure className="lg:w-9/12 ml-auto">
-                        <Image src={coverImage as string} alt="cover_image" width={1920} height={1080} className="float-right rounded-lg" />
+                        <Image
+                            src={coverImage as string}
+                            alt="cover_image"
+                            width={1920}
+                            height={1080}
+                            className="float-right rounded-lg"
+                        />
                     </figure>
                 </div>
             </Link>
@@ -44,21 +85,31 @@ export default function PostContainer({ coverImage, title, titleId, description,
                 <div className="flex-1">
                     <div className="flex items-center gap-2">
                         <div className="flex items-center gap-2">
-                            <Link href={`/${authorUsername ? authorUsername : userId}/${titleId}`}><FontAwesomeIcon icon={faHeart} size="lg" title="Reactions" /></Link>
-                            <div>
-                                0
-                            </div>
+                            <Link
+                                href={`/${
+                                    authorUsername ? authorUsername : userId
+                                }/${titleId}`}
+                            >
+                                <FontAwesomeIcon
+                                    icon={faHeart}
+                                    size="lg"
+                                    title="Reactions"
+                                />
+                            </Link>
+                            <div>{_count?.reactions}</div>
                         </div>
                         <div className="flex items-center gap-2">
-                            <FontAwesomeIcon icon={faComment} size="lg" title="Comments" />
-                            <div>
-                                0
-                            </div>
+                            <FontAwesomeIcon
+                                icon={faComment}
+                                size="lg"
+                                title="Comments"
+                            />
+                            <div>{_count?.comments}</div>
                         </div>
                     </div>
                 </div>
-                <PostBookmark titleId={titleId} faSize={'lg'} />
+                <PostBookmark titleId={titleId} faSize={"lg"} />
             </div>
         </div>
-    )
+    );
 }

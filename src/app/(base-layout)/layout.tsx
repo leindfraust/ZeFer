5a6f4c6d017685cbf374d61e8aca66340fbcf3dc
Leindfraust <rozer223@gmail.com>
2023-12-13 19:46:46 +0800

    style: prettier

--- src/app/(base-layout)/layout.tsx
+++ src/app/(base-layout)/layout.tsx
@@ -1,32 +1,38 @@
-import Navigation from '@/components/Navigation';
+import Navigation from "@/components/Navigation";
 
-import { authConfig } from '@/utils/authConfig';
-import { getServerSession } from 'next-auth';
-import prisma from '@/db';
-import { User } from '@prisma/client';
+import { authConfig } from "@/utils/authConfig";
+import { getServerSession } from "next-auth";
+import prisma from "@/db";
+import { User } from "@prisma/client";
+import QueryWrapper from "@/components/QueryWrapper";
+import NextAuthProvider from "@/components/provider/NextAuthProvider";
 
 export default async function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
+    const session = await getServerSession(authConfig);
 
-  const session = await getServerSession(authConfig)
+    const user = (await prisma.user.findUnique({
+        where: { id: session?.user.id ?? "" },
+        select: {
+            name: true,
+            image: true,
+            id: true,
+            username: true,
+        },
+    })) as User;
 
-  const user = await prisma.user.findUnique({
-    where: { id: session?.user.id ?? '' },
-    select: {
-      name: true,
-      image: true,
-      id: true,
-      username: true
-    }
-  }) as User
-
-  return (<>
-    <Navigation {...user} />
-    {children}
-    {/* <footer className="footer p-10 bg-base-200 text-base-content">
+    return (
+        <>
+            <QueryWrapper>
+                <NextAuthProvider>
+                    <Navigation {...user} />
+                </NextAuthProvider>
+            </QueryWrapper>
+            {children}
+            {/* <footer className="footer p-10 bg-base-200 text-base-content">
           <aside>
             <h1 className=' text-7xl'>ZeFer</h1>
             <p>Tell your story to the world.</p>
@@ -52,6 +58,6 @@ export default async function RootLayout({
             <a className="link link-hover">Cookie policy</a>
           </nav>
         </footer> */}
-  </>
-  )
+        </>
+    );
 }

8764386d6d7cbfd2398b2ace9a6603fd7a6396f4
Leindfraust <rozer223@gmail.com>
2023-12-13 19:48:11 +0800

    refactor: removed comment

--- src/socket.ts
+++ src/socket.ts
@@ -5,7 +5,6 @@ let socketInstance: Socket | null = null;
 
 const useSocket = () => {
     if (!socketInstance) {
-        // Replace 'http://localhost:3000' with your server URL
         socketInstance = io(URL, {
             auth: {
                 token: process.env.NEXT_PUBLIC_SOCKET_SERVER_SECRET,

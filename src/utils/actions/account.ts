936913af2b0a57344c68cfeb15fec74567afbd41
Leindfraust <rozer223@gmail.com>
2024-01-03 09:02:50 +0800

    feat: added API Keys component

--- src/utils/actions/account.ts
+++ src/utils/actions/account.ts
@@ -1,41 +1,72 @@
-"use server"
+"use server";
 
 import { getServerSession } from "next-auth";
 import { authConfig } from "../authConfig";
 import prisma from "@/db";
+import { init } from "@paralleldrive/cuid2";
 
 export async function unlinkAccount(accountId: string, providerId: string) {
-
-    const session = await getServerSession(authConfig)
+    const session = await getServerSession(authConfig);
 
     try {
         const linkedProviders = await prisma.account.count({
-            where: { userId: session?.user.id }
-        })
+            where: { userId: session?.user.id },
+        });
         if (linkedProviders > 1) {
             const unlinkProviderAccount = await prisma.account.delete({
                 where: {
                     id: accountId,
-                    providerAccountId: providerId
-                }
-            })
-            if (unlinkProviderAccount) return true
+                    providerAccountId: providerId,
+                },
+            });
+            if (unlinkProviderAccount) return true;
         } else {
-            throw new Error('Cannot perform operation, user has only 1 linked account.')
+            throw new Error(
+                "Cannot perform operation, user has only 1 linked account.",
+            );
         }
     } catch (error) {
-        return error
+        return error;
     }
 }
 
 export async function deleteUser() {
-    const session = await getServerSession(authConfig)
+    const session = await getServerSession(authConfig);
     try {
         const deleteUserAccount = await prisma.user.delete({
-            where: { id: session?.user.id }
-        })
-        if (deleteUserAccount) return true
+            where: { id: session?.user.id },
+        });
+        if (deleteUserAccount) return true;
     } catch (error) {
-        return error
+        return error;
     }
+}
+
+export async function generateApiKey(apiName: string) {
+    const createdId = init({
+        length: 48,
+    });
+    const apiKey = `sk-${createdId()}`;
+    const session = await getServerSession(authConfig);
+    const addApiKey = await prisma.apiKey.create({
+        data: {
+            name: apiName,
+            key: apiKey,
+            user: {
+                connect: {
+                    id: session?.user.id,
+                },
+            },
+        },
+    });
+    if (addApiKey) return addApiKey;
+}
+
+export async function revokeApiKey(apiKeyId: string) {
+    const revokeApiKey = await prisma.apiKey.delete({
+        where: {
+            id: apiKeyId,
+        },
+    });
+    if (revokeApiKey) return true;
 }

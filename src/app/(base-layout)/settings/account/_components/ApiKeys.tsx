7835462adf11c41111206e8dd7d95a060f575ef9
Ronan <rozer223@gmail.com>
2024-01-22 14:59:42 +0800

    refactor: ability add or remove API Key

--- src/app/(base-layout)/settings/account/_components/ApiKeys.tsx
+++ src/app/(base-layout)/settings/account/_components/ApiKeys.tsx
@@ -3,10 +3,13 @@
 import Input from "@/components/ui/Input";
 import Modal from "@/components/ui/Modal";
 import { FormContext } from "@/types/formContext";
-import { generateApiKey } from "@/utils/actions/account";
+import { generateApiKey, revokeApiKey } from "@/utils/actions/account";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ApiKey } from "@prisma/client";
 import { useRef, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 
 export default function ApiKeys({
     initialApiKeys,
@@ -14,7 +17,11 @@ export default function ApiKeys({
     initialApiKeys: ApiKey[];
 }) {
     const [apiKeys, setApiKeys] = useState<ApiKey[]>(initialApiKeys);
+
     const createApiModalRef = useRef<HTMLDialogElement>(null);
+    const deleteApiModalRef = useRef<HTMLDialogElement>(null);
+
+    const [toDelApiKeyId, setToDelApiKeyId] = useState<string>();
 
     const form = useForm();
 
@@ -29,13 +36,24 @@ export default function ApiKeys({
         },
     };
 
+    function delApiKeyModalPrompt({ id }: ApiKey) {
+        setToDelApiKeyId(id);
+        deleteApiModalRef.current?.show();
+    }
+
     const addApiKey = form.handleSubmit(async (data) => {
         const duplicateName = apiKeys.find(
             (apiKey) => apiKey.name === data.Name,
         );
         if (!duplicateName) {
             const generatedApiKey = await generateApiKey(data.Name);
-            if (generatedApiKey) setApiKeys([...apiKeys, generatedApiKey]);
+            if (generatedApiKey) {
+                setApiKeys([...apiKeys, generatedApiKey]);
+                toast.success("API key successfully created", {
+                    id: "123",
+                });
+                createApiModalRef.current?.close();
+            }
         } else {
             form.setError("Name", {
                 type: "uniqueConstraint",
@@ -43,6 +61,20 @@ export default function ApiKeys({
             });
         }
     });
+
+    const deleteApiKey = async () => {
+        if (toDelApiKeyId) {
+            const deleteApiKey = await revokeApiKey(toDelApiKeyId);
+            if (deleteApiKey) {
+                setApiKeys([...deleteApiKey]);
+                toast.success("API Key successfully deleted", {
+                    id: "123",
+                });
+            }
+        }
+        deleteApiModalRef.current?.close();
+    };
+
     return (
         <div className="pt-4">
             <div className="flex items-center gap-2">
@@ -54,6 +86,7 @@ export default function ApiKeys({
                     Create
                 </button>
             </div>
+
             <Modal ref={createApiModalRef}>
                 <h2 className="text-lg font-bold">Create New Secret Key</h2>
                 <FormProvider {...form}>
@@ -68,12 +101,27 @@ export default function ApiKeys({
                     </button>
                 </div>
             </Modal>
+
+            <Modal ref={deleteApiModalRef}>
+                <h2 className="text-lg font-bold">
+                    Are you sure you want to delete{" "}
+                </h2>
+                <div className="modal-action">
+                    <form method="dialog">
+                        <button className="btn">Cancel</button>
+                    </form>
+                    <button className="btn btn-neutral" onClick={deleteApiKey}>
+                        Confirm
+                    </button>
+                </div>
+            </Modal>
+
             {apiKeys.length !== 0 ? (
                 <div className="overflow-x-auto">
                     <table className="table">
-                        {/* head */}
                         <thead>
                             <tr>
+                                <th></th>
                                 <th>NAME</th>
                                 <th>KEY</th>
                                 <th>CREATED</th>
@@ -81,9 +129,20 @@ export default function ApiKeys({
                             </tr>
                         </thead>
                         <tbody>
-                            {/* row 1 */}
                             {apiKeys.map((apiKey) => (
                                 <tr key={apiKey.id}>
+                                    <th>
+                                        {
+                                            <FontAwesomeIcon
+                                                icon={faTrash}
+                                                color="red"
+                                                cursor={"pointer"}
+                                                onClick={() =>
+                                                    delApiKeyModalPrompt(apiKey)
+                                                }
+                                            />
+                                        }
+                                    </th>
                                     <th>{apiKey.name}</th>
                                     <td>{apiKey.key}</td>
                                     <td>

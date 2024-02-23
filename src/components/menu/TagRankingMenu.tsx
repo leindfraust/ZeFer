b68e472e2b64a785933a99943ec7bf34523fc2e2
Ronan <rozer223@gmail.com>
2024-02-23 19:13:58 +0800

    fix: make sure only 10 tags will be output on tag rankings

--- src/components/menu/TagRankingMenu.tsx
+++ src/components/menu/TagRankingMenu.tsx
@@ -1,30 +1,45 @@
-'use client'
+"use client";
 
 import { TagRank } from "@/types/tag";
 import { getTagRankings } from "@/utils/actions/tag";
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { TagsRanking } from "@prisma/client";
+import { JsonValue } from "@prisma/client/runtime/library";
 import Link from "next/link";
 import { Fragment, useEffect, useState } from "react";
 
 export default function TagRankingMenu() {
-    const [tagsRanking, setTagsRanking] = useState<TagsRanking | null>()
+    const [tagsRanking, setTagsRanking] = useState<
+        TagsRanking[] | JsonValue[]
+    >();
 
     useEffect(() => {
-        getTagRankings().then(response => setTagsRanking(response))
-    }, [])
+        getTagRankings().then((response) => setTagsRanking(response));
+    }, []);
 
-    return (<>
-        <div className="flex items-center space-x-1 mb-4">
-            <h3 className="text-lg">Trending tags</h3>
-            <FontAwesomeIcon icon={faFire} />
-        </div>
-        <ul className="space-y-2 text-sm font-bold">
-            {tagsRanking && (tagsRanking.data as TagRank[]).map((tag: TagRank, index) => (
-                <Fragment key={index}>
-                    <li><Link href={`/tag/${tag.tag}`}>#{tag.tag}</Link></li>
-                </Fragment>
-            ))}
-        </ul></>)
+    return (
+        <>
+            <div className="flex items-center space-x-1 mb-4">
+                <h3 className="text-lg">Trending tags</h3>
+                <FontAwesomeIcon icon={faFire} />
+            </div>
+            <ul className="space-y-2 text-sm font-bold">
+                {tagsRanking &&
+                    (tagsRanking as TagRank[]).map((tag: TagRank, index) => (
+                        <>
+                            {index <= 9 && (
+                                <Fragment key={index}>
+                                    <li>
+                                        <Link href={`/tag/${tag.tag}`}>
+                                            #{tag.tag}
+                                        </Link>
+                                    </li>
+                                </Fragment>
+                            )}
+                        </>
+                    ))}
+            </ul>
+        </>
+    );
 }
--- src/utils/actions/tag.ts
+++ src/utils/actions/tag.ts
@@ -1,30 +1,32 @@
-"use server"
+"use server";
 
-import { authConfig } from "@/utils/authConfig"
-import prisma from "@/db"
-import { getServerSession } from "next-auth"
-import OpenAI from "openai"
+import { authConfig } from "@/utils/authConfig";
+import prisma from "@/db";
+import { getServerSession } from "next-auth";
+import OpenAI from "openai";
 
 export async function getTagRankings() {
     const tagsRanking = await prisma.tagsRanking.findFirst({
         orderBy: {
-            createdAt: 'desc'
+            createdAt: "desc",
         },
-        take: 1
-    })
-    return tagsRanking
+        take: 1,
+    });
+    return tagsRanking?.data.slice(0, 10);
 }
 
 export async function updateInterest(tag: string) {
-    const session = await getServerSession(authConfig)
+    const session = await getServerSession(authConfig);
     try {
         const getUser = await prisma.user.findUnique({
             where: {
-                id: session?.user.id
-            }
-        })
+                id: session?.user.id,
+            },
+        });
         //if user hasn't followed the tag
-        const ifNotFollowing = getUser?.interests.filter((interest: string) => interest === tag)
+        const ifNotFollowing = getUser?.interests.filter(
+            (interest: string) => interest === tag,
+        );
         if (ifNotFollowing?.length === 0) {
             const updateUser = await prisma.user.update({
                 where: {
@@ -32,56 +34,62 @@ export async function updateInterest(tag: string) {
                 },
                 data: {
                     interests: {
-                        push: tag
-                    }
-                }
-            })
-            if (updateUser) return 'following'
+                        push: tag,
+                    },
+                },
+            });
+            if (updateUser) return "following";
         }
         // unfollow tag
-        const newInterests = [...(getUser?.interests as string[]).filter(interest => interest !== tag)]
+        const newInterests = [
+            ...(getUser?.interests as string[]).filter(
+                (interest) => interest !== tag,
+            ),
+        ];
         const updateUser = await prisma.user.update({
             where: {
                 id: session?.user.id,
             },
             data: {
-                interests: newInterests
-            }
-        })
-        if (updateUser) return 'unfollowing'
+                interests: newInterests,
+            },
+        });
+        if (updateUser) return "unfollowing";
     } catch (err) {
-        console.log(err)
-        return err
+        console.log(err);
+        return err;
     }
 }
 
 export async function ifTagFollowing(tag: string) {
-    const session = await getServerSession(authConfig)
+    const session = await getServerSession(authConfig);
     const tagFollowed = await prisma.user.findUnique({
         where: {
             id: session?.user.id,
             interests: {
-                has: tag
-            }
-        }
-    })
-    if (tagFollowed) return true
-    return false
+                has: tag,
+            },
+        },
+    });
+    if (tagFollowed) return true;
+    return false;
 }
 
 export async function validateTag(tag: string) {
     const openai = new OpenAI({
-        apiKey: process.env.OPEN_AI_KEY
-    })
+        apiKey: process.env.OPEN_AI_KEY,
+    });
 
     const validate = await openai.chat.completions.create({
-        messages: [{
-            role: 'assistant',
-            content: `You are a helpful assistant and I want you to validate the following keyword for tag creation. Follow the rules: 1. A tag must not contain any malicious word in any languages. 2. You will only output true or false. Now validate the tag: ${tag}`
-        }],
+        messages: [
+            {
+                role: "assistant",
+                content: `You are a helpful assistant and I want you to validate the following keyword for tag creation. Follow the rules: 1. A tag must not contain any malicious word in any languages. 2. You will only output true or false. Now validate the tag: ${tag}`,
+            },
+        ],
         model: "gpt-3.5-turbo",
-    })
-    const result = validate.choices[0].message.content
-    if (result?.toLowerCase().includes("true")) return true
-    return false
+    });
+    const result = validate.choices[0].message.content;
+    if (result?.toLowerCase().includes("true")) return true;
+    return false;
 }

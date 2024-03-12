8e2ff886ab6d9f05a41eabf7250692f2e3652973
Ronan <rozer223@gmail.com>
2024-03-12 18:42:39 +0800

    refactor: improved SearchMenu

--- src/app/(base-layout)/search/layout.tsx
+++ src/app/(base-layout)/search/layout.tsx
@@ -1,64 +1,64 @@
+"use client";
+
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
+const SearchMenu = () => {
+    const pathName = usePathname();
+    const searchParams = useSearchParams();
+    const keyword = searchParams.get("q");
+
+    const links = [
+        {
+            name: "Posts",
+            href: `/search/posts?q=${keyword}`,
+            active: pathName === "/search/posts",
+        },
+        {
+            name: "People",
+            href: `/search/people?q=${keyword}`,
+            active: pathName === "/search/people",
+        },
+        {
+            name: "Tags",
+            href: `/search/tags?q=${keyword}`,
+            active: pathName === "/search/tags",
+        },
+        {
+            name: "My Posts",
+            href: `/search/my-posts?q=${keyword}`,
+            active: pathName === "/search/my-posts",
+        },
+    ];
+    return (
+        <ul className="menu menu-lg rounded-box">
+            {links.map((link, index) => (
+                <li key={index}>
+                    <Link
+                        href={link.href}
+                        className={`${link.active ? "active" : ""}`}
+                    >
+                        {link.name}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 export default function SearchLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    const pathName = usePathname();
-    const searchParams = useSearchParams();
-    const keyword = searchParams.get("q");
     return (
         <div className="mt-12 mb-12 lg:mr-28 lg:ml-28 mx-auto">
             <div className="lg:flex justify-center">
                 <div className="p-4 mx-auto w-1/2 lg:w-1/6">
-                    <ul className="menu menu-lg rounded-box">
-                        <li>
-                            <Link
-                                href={`/search/posts?q=${keyword}`}
-                                className={`${
-                                    pathName === "/search/posts" ? "active" : ""
-                                }`}
-                            >
-                                Posts
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href={`/search/people?q=${keyword}`}
-                                className={`${
-                                    pathName === "/search/people"
-                                        ? "active"
-                                        : ""
-                                }`}
-                            >
-                                People
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href={`/search/tags?q=${keyword}`}
-                                className={`${
-                                    pathName === "/search/tags" ? "active" : ""
-                                }`}
-                            >
-                                Tags
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                href={`/search/my-posts?q=${keyword}`}
-                                className={`${
-                                    pathName === "/search/my-posts"
-                                        ? "active"
-                                        : ""
-                                }`}
-                            >
-                                My Posts
-                            </Link>
-                        </li>
-                    </ul>
+                    <Suspense>
+                        <SearchMenu />
+                    </Suspense>
                 </div>
                 <div className="flex-1 ml-4 mr-4">{children}</div>
             </div>

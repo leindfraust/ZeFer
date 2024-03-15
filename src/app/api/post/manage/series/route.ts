df41da08d9f24017c05049c9a50ccfe7f80a5dfd
Ronan <rozer223@gmail.com>
2024-03-16 01:31:43 +0800

    chore: prettier

--- src/app/api/post/manage/series/route.ts
+++ src/app/api/post/manage/series/route.ts
@@ -4,89 +4,88 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(): Promise<any> {
-    const session = await getServerSession(authConfig)
+    const session = await getServerSession(authConfig);
     try {
         const getSeries = await prisma.postSeries.findMany({
             where: {
-                authorId: session?.user.id
+                authorId: session?.user.id,
             },
             include: {
-                posts: true
+                posts: true,
             },
             orderBy: {
-                updatedAt: 'desc'
-            }
-        })
-        return NextResponse.json({ data: getSeries }, { status: 200 })
+                updatedAt: "desc",
+            },
+        });
+        return NextResponse.json({ data: getSeries }, { status: 200 });
     } catch (err) {
-        return NextResponse.json({ err }, { status: 500 })
+        return NextResponse.json({ err }, { status: 500 });
     }
 }
 
 export async function PUT(req: NextRequest): Promise<any> {
-    const session = await getServerSession(authConfig)
-    const url = new URL(req.url)
-    const seriesId = url.searchParams.get("seriesId") as string
+    const session = await getServerSession(authConfig);
+    const url = new URL(req.url);
+    const seriesId = url.searchParams.get("seriesId") as string;
 
-    const body = await req.formData()
-    const title = body.get('title') as string
-    const description = body.get('description') as string
+    const body = await req.formData();
+    const title = body.get("title") as string;
+    const description = body.get("description") as string;
     try {
         const postSeries = await prisma.postSeries.update({
             where: {
                 authorId: session?.user.id,
-                id: seriesId
+                id: seriesId,
             },
             data: {
                 title,
-                description
-            }
-        })
-        if (postSeries) return NextResponse.json({ status: 200 })
+                description,
+            },
+        });
+        if (postSeries) return NextResponse.json({ status: 200 });
     } catch (err) {
-        return NextResponse.json({ err }, { status: 500 })
+        return NextResponse.json({ err }, { status: 500 });
     }
 }
 
 export async function POST(req: NextRequest): Promise<any> {
-    const session = await getServerSession(authConfig)
-    const body = await req.formData()
-    const title = body.get('title') as string
-    const description = body.get('description') as string
+    const session = await getServerSession(authConfig);
+    const body = await req.formData();
+    const title = body.get("title") as string;
+    const description = body.get("description") as string;
 
     try {
         const postSeries = await prisma.postSeries.create({
             data: {
                 title: title,
-                description: description as string || '',
+                description: (description as string) || "",
                 author: {
                     connect: {
-                        id: session?.user.id
-                    }
-                }
-            }
-        })
-        console.log(postSeries)
-        if (postSeries) return NextResponse.json({ status: 200 })
+                        id: session?.user.id,
+                    },
+                },
+            },
+        });
+        if (postSeries) return NextResponse.json({ status: 200 });
     } catch (err) {
-        return NextResponse.json({ err }, { status: 200 })
+        return NextResponse.json({ err }, { status: 200 });
     }
 }
 
 export async function DELETE(req: NextRequest): Promise<any> {
-    const session = await getServerSession(authConfig)
-    const url = new URL(req.url)
-    const seriesId = url.searchParams.get("seriesId")
+    const session = await getServerSession(authConfig);
+    const url = new URL(req.url);
+    const seriesId = url.searchParams.get("seriesId");
 
     try {
         const deleteSeries = await prisma.postSeries.delete({
             where: {
                 authorId: session?.user.id,
-                id: seriesId as string
-            }
-        })
-        if (deleteSeries) return NextResponse.json({ status: 200 })
+                id: seriesId as string,
+            },
+        });
+        if (deleteSeries) return NextResponse.json({ status: 200 });
     } catch (err) {
-        return NextResponse.json({ err }, { status: 500 })
+        return NextResponse.json({ err }, { status: 500 });
     }
 }

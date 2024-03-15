9b294b84296759519eece8b4877f5ad4c4826990
Ronan <rozer223@gmail.com>
2024-03-16 01:15:17 +0800

    refactor: NotificationLink will be using LinkMenu

---
+++ src/components/ui/LinkMenu.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { MenuLink } from "@/types/menu";
+
+export default function LinkMenu({
+    links,
+}: {
+    relativePathname: string;
+    links: MenuLink[];
+}) {
+    const router = useRouter();
+    const pathName = usePathname();
+    const [selectedLink, setSelectedLink] = useState<string>(pathName);
+
+    function selectLink(e: React.ChangeEvent<HTMLSelectElement>) {
+        const splitLink = e.currentTarget.value.split("?")[0];
+        if (pathName !== splitLink) {
+            setSelectedLink(e.currentTarget.value);
+            router.push(e.currentTarget.value);
+        }
+    }
+
+    function activeLink(href: string) {
+        const splitLink = href.split("?")[0];
+        if (pathName === splitLink) return "active";
+    }
+
+    return (
+        <>
+            <ul className="hidden lg:block menu menu-lg rounded-box">
+                {links.map((link) => (
+                    <li key={link.href}>
+                        <Link
+                            href={link.href}
+                            onClick={() => setSelectedLink(link.href)}
+                            className={activeLink(link.href)}
+                        >
+                            {link.label}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            <select
+                className="select select-bordered font-bold text-lg w-full max-w-xs lg:hidden"
+                value={selectedLink}
+                onChange={selectLink}
+            >
+                {links.map((link) => (
+                    <option key={link.label} value={link.href}>
+                        {link.label}
+                    </option>
+                ))}
+            </select>
+        </>
+    );
+}

65a9b4078c8ba54cc8f57537db634ff27304e4d5
Ronan <rozer223@gmail.com>
2024-03-31 20:39:47 +0800

    refactor: disable sentry in dev env

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,11 +6,12 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://d11d24a221ab5241ed52aa11667de2f9@o4506060501614592.ingest.sentry.io/4506200102207488",
+    dsn: "https://d11d24a221ab5241ed52aa11667de2f9@o4506060501614592.ingest.sentry.io/4506200102207488",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+    enabled: process.env.NODE_ENV === "production",
 });

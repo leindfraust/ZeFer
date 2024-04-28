fd580660d98c08f19a747cd18ba6ff84a50c6714
Ronan <rozer223@gmail.com>
2024-04-28 11:19:20 +0800

    feat: added organization creation
    Only creation of organization, no organization will be displayed yet.

---
+++ src/app/(base-layout)/manage/organization/page.tsx
@@ -0,0 +1,5 @@
+import OrganizationManageContainer from "./_components/OrganizationManageContainer";
+
+export default async function ManageOrganizations() {
+    return <OrganizationManageContainer />;
+}

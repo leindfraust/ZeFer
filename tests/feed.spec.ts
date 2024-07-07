07c9204e8706e0f8e492c06a459b7b1eb4a7dd97
Ronan <rozer223@gmail.com>
2024-07-07 10:17:48 +0800

    fix: wait deployment status ready and test on staging

--- tests/feed.spec.ts
+++ tests/feed.spec.ts
@@ -1,28 +1,30 @@
 import { test, expect } from "@playwright/test";
 
 test("can load default to relevance feed", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
-    await page.goto("http://localhost:3000/?feed=relevance");
-    await expect(page).toHaveURL("http://localhost:3000/?feed=relevance");
+    await page.goto("https://staging.zefer.blog/");
+    await page.goto("https://staging.zefer.blog/?feed=relevance");
+    await expect(page).toHaveURL("https://staging.zefer.blog/?feed=relevance");
 });
 
 test("can load relevance feed", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
-    await page.goto("http://localhost:3000/?feed=latest");
+    await page.goto("https://staging.zefer.blog/");
+    await page.goto("https://staging.zefer.blog/?feed=latest");
     await page.getByRole("heading", { name: "Relevant" }).click();
-    await expect(page).toHaveURL("http://localhost:3000/?feed=relevance");
+    await expect(page).toHaveURL("https://staging.zefer.blog/?feed=relevance");
 });
 
 test("can load latest feed", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
-    await page.goto("http://localhost:3000/?feed=relevance");
+    await page.goto("https://staging.zefer.blog/");
+    await page.goto("https://staging.zefer.blog/?feed=relevance");
     await page.getByRole("heading", { name: "Latest" }).click();
-    await expect(page).toHaveURL("http://localhost:3000/?feed=latest");
+    await expect(page).toHaveURL("https://staging.zefer.blog/?feed=latest");
 });
 
 test("can load most-popular feed", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
-    await page.goto("http://localhost:3000/?feed=relevance");
+    await page.goto("https://staging.zefer.blog/");
+    await page.goto("https://staging.zefer.blog/?feed=relevance");
     await page.getByRole("heading", { name: "Most Popular" }).click();
-    await expect(page).toHaveURL("http://localhost:3000/?feed=most-popular");
+    await expect(page).toHaveURL(
+        "https://staging.zefer.blog/?feed=most-popular",
+    );
 });

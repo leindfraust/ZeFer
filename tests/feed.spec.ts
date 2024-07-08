db92b59b67306c28a12fed6176bb451a0802c5aa
Ronan <rozer223@gmail.com>
2024-07-08 20:25:39 +0800

    refactor: adjust timeout and baseUrl

--- tests/feed.spec.ts
+++ tests/feed.spec.ts
@@ -3,27 +3,27 @@ const baseUrl = process.env.BASE_URL || "https://staging.zefer.blog/";
 
 test("can load default to relevance feed", async ({ page }) => {
     await page.goto(baseUrl);
-    await page.goto(`${baseUrl}/?feed=relevance`);
-    await expect(page).toHaveURL(`${baseUrl}/?feed=relevance`);
+    await page.goto(`${baseUrl}?feed=relevance`);
+    await expect(page).toHaveURL(`${baseUrl}?feed=relevance`);
 });
 
 test("can load relevance feed", async ({ page }) => {
     await page.goto(baseUrl);
-    await page.goto(`${baseUrl}/?feed=latest`);
+    await page.goto(`${baseUrl}?feed=latest`);
     await page.getByRole("heading", { name: "Relevant" }).click();
-    await expect(page).toHaveURL(`${baseUrl}/?feed=relevance`);
+    await expect(page).toHaveURL(`${baseUrl}?feed=relevance`);
 });
 
 test("can load latest feed", async ({ page }) => {
     await page.goto(baseUrl);
-    await page.goto(`${baseUrl}/?feed=relevance`);
+    await page.goto(`${baseUrl}?feed=relevance`);
     await page.getByRole("heading", { name: "Latest" }).click();
-    await expect(page).toHaveURL(`${baseUrl}/?feed=latest`);
+    await expect(page).toHaveURL(`${baseUrl}?feed=latest`);
 });
 
 test("can load most-popular feed", async ({ page }) => {
     await page.goto(baseUrl);
-    await page.goto(`${baseUrl}/?feed=relevance`);
+    await page.goto(`${baseUrl}?feed=relevance`);
     await page.getByRole("heading", { name: "Most Popular" }).click();
-    await expect(page).toHaveURL(`${baseUrl}/?feed=most-popular`);
+    await expect(page).toHaveURL(`${baseUrl}?feed=most-popular`);
 });
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test("able to search", async ({ page }) => {
-    const baseUrl = process.env.BASE_URL || "https://staging.zefer.blog";
+    const baseUrl = process.env.BASE_URL || "https://staging.zefer.blog/";
     await page.goto(baseUrl);
-    await page.goto(`${baseUrl}/?feed=relevance`);
+    await page.goto(`${baseUrl}?feed=relevance`);
     await page.getByRole("textbox", { name: "Search…" }).click();
     await page.getByRole("textbox", { name: "Search…" }).fill("test");
     await page.getByRole("textbox", { name: "Search…" }).press("Enter");
-    await page.goto(`${baseUrl}/search/posts?q=test&feed=relevance`);
+    await page.goto(`${baseUrl}search/posts?q=test&feed=relevance`);
     await expect(page.getByText("RelevantLatestMost Popular")).toBeVisible();
     await expect(page.getByRole("link", { name: "People" })).toBeVisible();
     await expect(page.getByRole("link", { name: "Tags" }).nth(1)).toBeVisible();

a472e8fad9b760c05623d8d411ea5667ba100de0
Ronan <rozer223@gmail.com>
2024-07-08 21:07:26 +0800

    fix: remove goto baseUrl
    Since the app will renavigate

--- tests/feed.spec.ts
+++ tests/feed.spec.ts
@@ -2,27 +2,23 @@ import { test, expect } from "@playwright/test";
 const baseUrl = process.env.BASE_URL || "https://staging.zefer.blog/";
 
 test("can load default to relevance feed", async ({ page }) => {
-    await page.goto(baseUrl);
     await page.goto(`${baseUrl}?feed=relevance`);
     await expect(page).toHaveURL(`${baseUrl}?feed=relevance`);
 });
 
 test("can load relevance feed", async ({ page }) => {
-    await page.goto(baseUrl);
     await page.goto(`${baseUrl}?feed=latest`);
     await page.getByRole("heading", { name: "Relevant" }).click();
     await expect(page).toHaveURL(`${baseUrl}?feed=relevance`);
 });
 
 test("can load latest feed", async ({ page }) => {
-    await page.goto(baseUrl);
     await page.goto(`${baseUrl}?feed=relevance`);
     await page.getByRole("heading", { name: "Latest" }).click();
     await expect(page).toHaveURL(`${baseUrl}?feed=latest`);
 });
 
 test("can load most-popular feed", async ({ page }) => {
-    await page.goto(baseUrl);
     await page.goto(`${baseUrl}?feed=relevance`);
     await page.getByRole("heading", { name: "Most Popular" }).click();
     await expect(page).toHaveURL(`${baseUrl}?feed=most-popular`);
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from "@playwright/test";
 
 test("able to search", async ({ page }) => {
     const baseUrl = process.env.BASE_URL || "https://staging.zefer.blog/";
-    await page.goto(baseUrl);
     await page.goto(`${baseUrl}?feed=relevance`);
     await page.getByRole("textbox", { name: "Search…" }).click();
     await page.getByRole("textbox", { name: "Search…" }).fill("test");

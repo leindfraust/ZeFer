83f6d5e8f66fde9d1ce0afff26ad80fd215775ea
Ronan <rozer223@gmail.com>
2024-07-08 20:48:59 +0800

    fix: make expect 20 mins and not fully parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,12 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
     timeout: 20 * 60 * 1000, // 20 minutes for staging build
+    expect: {
+        timeout: 20 * 60 * 1000, // 20 minutes for staging build
+    },
     testDir: "./tests",
     /* Run tests in files in parallel */
-    fullyParallel: true,
+    // fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */

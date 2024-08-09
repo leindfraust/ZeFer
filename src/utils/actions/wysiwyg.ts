fec17fe6a5db5b5b02182b1ea03ccdbbe62b24fa
Ronan <rozer223@gmail.com>
2024-08-09 09:09:44 +0800

    WIP

--- src/utils/actions/wysiwyg.ts
+++ src/utils/actions/wysiwyg.ts
@@ -7,16 +7,18 @@ export async function autocompleteGemini(words: string) {
         const genAI = new GoogleGenerativeAI(
             process.env.GOOGLE_AI_KEY as string,
         );
-        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-        const prompt = `You are an autocomplete bot. I will give a phrase or a random of words and you will try to complete it based on the context. 
-    Here are the rules: 
+        const model = genAI.getGenerativeModel({
+            model: "gemini-1.5-flash-latest",
+        });
+        const prompt = `Complete the following phrase based on its context. Only output the next words if the phrase can be autocompleted. If not, reply with "false". 
+
+    Rules:
     
-    1. You will only output the next words if the set of words can be autocompleted.
-    2. If the set of words can be autocompleted, reply through outputting the next words and if don't, reply with a word "false".
-    3. You will not process any words that are indecent, offensive, or malicious.
-    4. The next words should be based on the context of the set of words and should be relevant. 
-    5. If the next words requires a space in the beginning, put a space in the beginning, just follow a proper format on the next words.
-    Here are the words: ${words}`;
+    1. Do not output indecent, offensive, or malicious content.
+    2.  The next words should be relevant and contextually appropriate.
+    3. If a space is needed before the next words, include it, if not leave it be.
+
+    Phrase: ${words}`;
         const result = await model.generateContent(prompt);
         const response = result.response;
         const text = response.text();
